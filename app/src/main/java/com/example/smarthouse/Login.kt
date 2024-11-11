fb880a2a483fa069300dbc38214fd61ff21a98bd
Luxa <Skofild2228@mail.ru>
2024-11-12 02:07:57 +0600

    home

--- app/src/main/java/com/example/smarthouse/Login.kt
+++ app/src/main/java/com/example/smarthouse/Login.kt
@@ -1,5 +1,6 @@
 package com.example.smarthouse
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,7 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val Intent : Intent = Intent(this, MainMenu::class.java)
+        startActivity(Intent)
     }
 }
